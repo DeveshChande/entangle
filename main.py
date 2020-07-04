8262a3877a81f83ad502921f13ca75d25cb4bab8
Devesh Chande <33889891+DeveshChande@users.noreply.github.com>
2020-07-04 12:20:54 +0530

    Fixed PublicKeyAuthentication Method
    Public Key Authentication Method now functions as desired. It logs a hexlify version of the key as well.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
+from binascii import hexlify
 import logging
 import paramiko
+from paramiko.py3compat import b, u, decodebytes
 import socket
 import sys
 import threading
@@ -11,7 +13,7 @@ def setup_logger(name, log_file, level=logging.INFO):
 
     file_handler = logging.FileHandler(log_file)
     formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
-    file_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)        
 
     logger = logging.getLogger(name)
     logger.setLevel(level)
@@ -30,7 +32,7 @@ class PasswordServer(paramiko.ServerInterface):
                 return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
         def check_auth_password(self, username, password):
-                uplist_logger.info(f'{username}\t{password}\n')
+                uplist_logger.info(f'PasswordAuth:{username}\t{password}\n')
 
                 return paramiko.AUTH_FAILED
 
@@ -38,15 +40,19 @@ class PublicKeyServer(paramiko.ServerInterface):
         def __init__(self):
                 self.event = threading.Event()
 
+        def get_allowed_auths(self, username):
+                return "password,publickey"
+
         def check_channel_request(self, kind, chanid):
                 if kind == 'session':
                         return paramiko.OPEN_SUCCEEDED
 
                 return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
-        def check_auth_password(self, username, password):
-                uplist_logger.info(f'{username}\t{password}\n')
+        def check_auth_publickey(self, username, key):
 
+                log_key = u(hexlify(key.get_fingerprint()))
+                uplist_logger.info(f'PublicKeyAuth:{username}\t{log_key}\n')
                 return paramiko.AUTH_FAILED
 
 class PasswordThreadedServer():
@@ -65,7 +71,7 @@ class PasswordThreadedServer():
                     if (t1-t0) > 30:
                         sys.exit()
                     threading.Thread(target = self.listenToClient,args = (clientsocket,address)).start()
-
+                    
                     iplist_logger.info(f'{str(address[0])}\t{str(address[1])}\n')
 
         def listenToClient(self, clientsocket, address):
@@ -91,12 +97,15 @@ class PasswordThreadedServer():
                         return False
 
 class PublicKeyThreadedServer():
+
         def __init__(self):
                 self.host = '192.168.56.104'
                 self.port = 22
                 self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 self.sock.bind((self.host, self.port))
+
+        
 
         def listen(self):
                 self.sock.listen(5)
@@ -106,7 +115,7 @@ class PublicKeyThreadedServer():
                     if (t1-t0) > 30:
                         sys.exit()
                     threading.Thread(target = self.listenToClient,args = (clientsocket,address)).start()
-
+                    
                     iplist_logger.info(f'{str(address[0])}\t{str(address[1])}\n')
 
         def listenToClient(self, clientsocket, address):
@@ -115,6 +124,7 @@ class PublicKeyThreadedServer():
                         ssh_connection = paramiko.Transport(clientsocket)
                         ssh_connection.add_server_key(HOST_KEY)
                         server = PublicKeyServer()
+                        print(address)
                         try:
                             ssh_connection.start_server(server=server)
                         except:
@@ -136,8 +146,8 @@ if __name__ == '__main__':
         parser = argparse.ArgumentParser()
         parser.add_argument("--password", "-P", help="Uses password to authenticate client", action="store_true")
         parser.add_argument("--key", "-K", help="Uses public key to authenticate client", action="store_true")
-
-        args = parser.parse_args()
+        
+        args = parser.parse_args()        
 
         uplist_logger = setup_logger('first_logger', 'uplist.log')
         iplist_logger = setup_logger('second_logger', 'iplist.log')
